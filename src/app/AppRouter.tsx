426b6e58eef9face0421dda71ec4dc9b801aa935
Pankov Dmitry <pankov.dmitry.dev@gmail.com>
2024-05-20 14:31:59 +0300

    router refactoring

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,19 +1,27 @@
-import Cart from "@/pages/Cart/Cart";
-import Store from "@/pages/Store/Store";
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+
+const Store = lazy(() => import("@/pages/Store/Store"));
+const Cart = lazy(() => import("@/pages/Cart/Cart"));
+
+const routes = [
+  { path: "/", element: <Store /> },
+  { path: "/link1", element: <div>link1</div> },
+  { path: "/link2", element: <div>link2</div> },
+  { path: "/link3", element: <div>link3</div> },
+  { path: "/link4", element: <div>link4</div> },
+  { path: "/Cart", element: <Cart /> },
+];
 
 function AppRouter() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<Store />} />
-        <Route path="/link1" element={<div>link1</div>} />
-        <Route path="/link2" element={<div>link2</div>} />
-        <Route path="/link3" element={<div>link3</div>} />
-        <Route path="/link4" element={<div>link4</div>} />
-        <Route path="/Cart" element={<Cart />} />
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
